3f9d208112fbeed17fc9e26df0bbcf60e59507dd
mylestoole <myles.toole001@umb.edu>
2021-07-21 15:54:47 -0400

    Fixed connecting multiple Pi's to one station. Having difficulty running bash scripts on my Windows.

--- Testbed_Scripts/Configuration/multi_connect.sh
+++ Testbed_Scripts/Configuration/multi_connect.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+read -p "First Pi: " firstpi
+read -p "Last Pi: " lastpi
 read -p "SSID: " uservar
 read -p "Password: " passvar 
 var=(107 108 109 110)
 
 for n in {1}
 do
-    for ip in "${var[@]}"
+    for ip in "{$firstpi..$lastpi}"
     do
 	gnome-terminal --command "ssh pi@10.1.1.$ip bash A_TO_C.sh $uservar $passvar"
     done
