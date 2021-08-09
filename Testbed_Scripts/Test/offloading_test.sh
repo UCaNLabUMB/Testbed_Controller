0b4926f30e1e5a4ce225614b41d7d5af6c6f89a2
humza-ali7 <humza.ali001@umb.edu>
2021-08-09 16:41:18 -0400

    updated offloading_test.sh to take in array parameters (still needs testing)

--- Testbed_Scripts/Test/offloading_test.sh
+++ Testbed_Scripts/Test/offloading_test.sh
@@ -3,22 +3,16 @@
 function run_tests {
 
 # Variables
-ip_server="192.168.1.229"
-ip_server2="192.168.2.200"
+ip_server="192.168"
 base_port=5000
 ip_prefix="10.1.1"
 trial=$1
 dur=$2
-r1=$3
-r2=$4
+
 
 # List of available IP
-declare -a num
-for (( i=$r1; i<=$r2; i++ ))
-do
-num+=($i)
-done
-echo ${num[@]}
+num=( "$3" )
+serv=( "$4" )
 
 # Type of iperf
 type="-c"
@@ -30,13 +24,19 @@ until [[ $counter -ge $trial ]]
 do
 	((counter++))
 	echo "This is trial " $counter
-	for i in "${num[@]}"; do
-		sshpass -p lightcomms5556 ssh pi@"$ip_prefix.$i" iperf $type $ip_server -t $dur | awk 'END{print $7","}'>> $ip_prefix.$i.csv &
+	for i in {1..${#num[@]}}; do
+		sshpass -p lightcomms5556 ssh pi@"$ip_prefix.${num[$i]}" iperf $type $ip_server.${serv[$i]} -t $dur | awk 'END{print $7","}'>> $ip_prefix.${num[$i]}.csv &
 	done
 	sleep $dur
 done
 echo "Test is finished"
 }
+
+function test {
+	new=( "$1" )
+	echo ${new[@]}
+}
+
 
 if [[ -z "$@" ]] 
 then
@@ -45,7 +45,7 @@ echo "Please enter the number of trials, duration (in seconds), range minimum an
 echo "Syntax: trials duration range1 range2--> 1 2 101 104"
 read -a input
 echo ${input[@]}
-run_tests ${input[0]} ${input[1]} ${input[2]} ${input[3]}
+run_tests ${input[0]} ${input[1]} "$(echo ${input[2]})" "$(echo ${input[3]})"
 
 else
 trial=0
@@ -60,6 +60,8 @@ while getopts ":d:t:s:i:r:" options; do
                 s ) set -f
                     IFS=' '
             	    ip=($OPTARG)
+		    echo ${ip[@]}
+		    ;;	
 
 		t ) trial=${OPTARG}
 			echo $trial
@@ -88,7 +90,8 @@ while getopts ":d:t:s:i:r:" options; do
 	esac
 done
 
-#run_tests $trial $dur $narr[@] $r2
+#test "$(echo ${ip[@]})"
+run_tests $trial $dur "$(echo ${narr[@]}" "$(echo ${ip[@]})"
 fi
 shift $((OPTIND -1))
 
