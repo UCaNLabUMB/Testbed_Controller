7594334fa7a88930a753a8d5a0303120a67fe93f
humza-ali7 <humza.ali001@umb.edu>
2021-08-10 15:53:06 -0400

    offloading_test.sh is now updated and tested

--- Testbed_Scripts/Test/offloading_test.sh
+++ Testbed_Scripts/Test/offloading_test.sh
@@ -1,48 +1,13 @@
 #!/bin/bash
 
-function run_tests {
-
-# Variables
-ip_server="192.168"
-base_port=5000
-ip_prefix="10.1.1"
-trial=$1
-dur=$2
-
-
-# List of available IP
-num=( "$3" )
-serv=( "$4" )
-
-# Type of iperf
-type="-c"
-
-# Iperf test for loop with number of tets and types
-#ssh pi@$ip_server screen -d -m iperf -s
-
-until [[ $counter -ge $trial ]]
-do
-	((counter++))
-	echo "This is trial " $counter
-	for i in {1..${#num[@]}}; do
-		sshpass -p lightcomms5556 ssh pi@"$ip_prefix.${num[$i]}" iperf $type $ip_server.${serv[$i]} -t $dur | awk 'END{print $7","}'>> $ip_prefix.${num[$i]}.csv &
-	done
-	sleep $dur
-done
-echo "Test is finished"
-}
-
-function test {
-	new=( "$1" )
-	echo ${new[@]}
-}
 
 
 if [[ -z "$@" ]] 
 then
 
 echo "Please enter the number of trials, duration (in seconds), range minimum and range maximum seperated by a space for this test."
-echo "Syntax: trials duration range1 range2--> 1 2 101 104"
+echo "Input the corresponding ip addresses for the servers that each pi will connect to. Follow the syntax below."
+echo "Syntax: trials duration range1 range2--> 1 2 "101 104" "1.200 2.200 1.200 2.200""
 read -a input
 echo ${input[@]}
 run_tests ${input[0]} ${input[1]} "$(echo ${input[2]})" "$(echo ${input[3]})"
@@ -86,16 +51,42 @@ while getopts ":d:t:s:i:r:" options; do
                     IFS=' '
             	    narr=($OPTARG)
             	    echo ${narr[@]}
+		    echo ${narr[0]}
+		    echo ${narr[1]}
             	    ;;
 	esac
 done
 
-#test "$(echo ${ip[@]})"
-run_tests $trial $dur "$(echo ${narr[@]}" "$(echo ${ip[@]})"
+for i in ${narr[@]}; do
+echo $i
+done
+#test "$(echo ${ip[@]})" "$(echo ${narr[@]})"
+#run_tests $trial $dur "$(echo ${narr[@]})" "$(echo ${ip[@]})"
+ip_server="192.168"
+base_port=5000
+ip_prefix="10.1.1"
+
+echo "${#narr[@]}"
+
+# Type of iperf
+type="-c"
+
+# Iperf test for loop with number of tets and types
+#ssh pi@$ip_server screen -d -m iperf -s
+
+until [[ $counter -ge $trial ]]
+do
+	((counter++))
+	echo "This is trial " $counter
+	for (( i=0; i<=(${#narr[@]}-1); i++ )); do
+		echo "Current Static IP address"
+		echo ${narr[$i]}
+		echo "Server IP"
+		echo ${ip[$i]}
+		ssh pi@"$ip_prefix.${narr[$i]}" iperf $type $ip_server.${ip[$i]} -t $dur | awk 'END{print $7","}'>> $ip_prefix.${narr[$i]}.csv &
+	done
+	sleep $dur
+done
+echo "Test is finished"
 fi
 shift $((OPTIND -1))
-
-
-
-
-
