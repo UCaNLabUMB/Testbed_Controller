e3ddd4b3069912f7db651bafb64dc1394bba3b9b
mylestoole <myles.toole001@umb.edu>
2021-08-12 13:31:47 -0400

    Modified get_ap.sh to use iwconfig and range of Pi's

--- Testbed_Scripts/Configuration/get_ap.sh
+++ Testbed_Scripts/Configuration/get_ap.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-id_prefix=10.1.1
-
-echo "Scanning first network" 
-for i in "109"
-do
-    ssh pi@"$id_prefix.$i" sudo arp-scan --interface=wlan0 --localnet
+var={$1..$2}
+for ip in $(seq $1 $2); do 
+	echo 10.1.1.$ip
+	ssh pi@10.1.1.$ip sudo iwconfig wlan0 | awk -F ':' '/ESSID:/ {print $2;}'
 done
-echo "Scanning second network"
-for j in "101"
-do
-    ssh pi@"$id_prefix.$j" sudo arp-scan --interface=wlan0 --localnet
-done
+exit
 
