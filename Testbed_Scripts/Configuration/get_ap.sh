0638c1d2a2b128082b7ad53160b5296b0ed34a8e
mylest7 <35745113+mylest7@users.noreply.github.com>
2022-05-13 09:57:01 -0400

    Update get_ap.sh

--- Testbed_Scripts/Configuration/get_ap.sh
+++ Testbed_Scripts/Configuration/get_ap.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# This script is meant to obtain the SSID's of the access points of each specified node 
+
 dev=()
 if [ -z "$@" ]
 then
-	
+
+# This section is meant to run if the script is run without any command line arguments that specify the range of nodes
 echo "Naming convention is 10.1.1.x"
 echo "Input each corresponding ip to be pinged"
 echo "i.e., for devices 10.1.1.101 to 10.1.1.104, input 101 102 103 104"
@@ -19,6 +22,8 @@ else
 while [ "$#" -gt 0 ];
 do
 case "$1" in 
+	# In order to obtain the SSID's of a large group of nodes, run this command with the flag -r and the maximum and minumum values of the node range. Ex: For nodes 101 
+	# through 106, run "bash get_ap.sh -r 101 for 106"
 	-r) var1="$2"; 
 	var2="$3";
 	i=$var1
@@ -27,6 +32,8 @@ case "$1" in
 	ssh pi@10.1.1.$i sudo iwconfig wlan0 | awk -F ':' '/ESSID:/ {print $2;}'
 	done
 	;;
+	# In order to obtain the SSID's of an individual node, run this command with the flag -l and the the specific node value. Ex: For node 101, 
+	# run "bash get_ap.sh -l 101"
 	-l) new=( "$@" )
 	echo ${new[@]}
 	for (( i=1; i<=${#new[@]}-1; i++))
