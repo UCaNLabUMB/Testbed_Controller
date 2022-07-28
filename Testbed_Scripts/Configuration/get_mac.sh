9ea0d5641d3c5da123d80bf93e9d75726f4e7577
Lenny <lennymartinez91@gmail.com>
2022-07-28 10:22:40 -0400

    Victoria updated network interface flag

--- Testbed_Scripts/Configuration/get_mac.sh
+++ Testbed_Scripts/Configuration/get_mac.sh
@@ -5,10 +5,11 @@
 #
 # Author: Victoria Planchart
 #
-# Description: This script indicates the MAC address of the given IP addresses.
+# Description: This script indicates the eth0, eth1 or wlan0 MAC address of the given IP addresses.
 #
 # Input: bash get_mac.sh enter, then enter the IP address suffix. 
 # Example: for IP:10.1.1.102 enter 102. For multiple IP addresses enter 102 103 104
+# 	   for specific network interface enter parameter "-n" followed by either eth0, eth1 or wlan0. e.g: bash get_mac.sh -n eth1
 #
 ####################################################################
 
@@ -20,23 +21,24 @@ read -a ip
 
 network_interface=eth0 # deffault
 
-while getopts 'hni:' OPTION; do
+while getopts 'hn:' OPTION; do
 	case "$OPTION" in
 		h)
 			help;;
-		ni)
+		n)
 			network_interface=$OPTARG;; 
 	esac
 done
 
-for i in "${ip[@]}"
+for i in "${ip[@]}" # loop through pis
 do
-	if ssh ucanlab@"10.1.1.$i" [ ! -d /sys/class/net/$network_interface ]
-	then
-		echo "Network Interface not found"
-	else
-		echo "$network_interface MAC Address for $i:"
-		ssh ucanlab@"10.1.1.$i" cat /sys/class/net/$network_interface/address
-	fi
-	i=$((i + 1))
+# if statement checks which pi is in server mode by looking for eth1 address
+if ssh ucanlab@"10.1.1.$i" [ ! -d /sys/class/net/$network_interface ]
+then
+	echo "Network Interface not found for $i"
+else
+	echo "$network_interface MAC Address for $i:"
+	ssh ucanlab@"10.1.1.$i" cat /sys/class/net/$network_interface/address
+fi
+i=$((i + 1))
 done
