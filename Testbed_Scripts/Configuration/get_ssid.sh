e8ae7efc240382c59a0f9f7a45deaf5dab8062c0
Lenny <lennymartinez91@gmail.com>
2022-07-28 10:35:16 -0400

    Victoria modified iwgetid command and username

--- Testbed_Scripts/Configuration/get_ssid.sh
+++ Testbed_Scripts/Configuration/get_ssid.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#################################################################### 
+# get_ssid.sh
+#
+# Initial Authors: Humza Ali / Myles Toole
+#
+# Description: This script is meant to check the status of all the nodes 
+#    in our system (whether or not they are connected to the control network)
+#
+# Input:
+#
+####################################################################
+
+
 # Th nodes in this sytem will have two connections to two separate networks (ethernet switch and Wi-Fi). This script will communicate with the nodes via ethenet connection
 # and will obtain the SSID of the access point that it is connected to before proper tests are started 
 
@@ -11,10 +24,10 @@ echo "Naming convention is 10.1.1.x"
 echo "Input each corresponding ip to obtain available SSIDs"
 echo "i.e., for devices 10.1.1.101 to 10.1.1.104, input 101 102 103 104"
 read -a ip
-echo ${ip[@]}
 for i in "${ip[@]}"
 do
-	ssh pi@"10.1.1.$i" nmcli dev wifi
+	echo "pi $i SSID"
+	ssh ucanlab@"10.1.1.$i" /usr/sbin/iwgetid -r
 done
 
 else
@@ -22,14 +35,14 @@ else
 while [ "$#" -gt 0 ];
 do
 case "$1" in 
-	# In order to check the SSID's of a large group of our nodes, run this command with the flag -r and the maximum and minumum values of 
+	# In order to check the SSIDs of a large group of our nodes, run this command with the flag -r and the maximum and minumum values of 
 	# the node range. Ex: For nodes 101 through 106, run "bash get_ssid.sh -r 101 for 106"-r) var1="$2";
 	-r) var1="$2"; 
 	var2="$3";
 	i=$var1
 	for (( i=$var1; i<=$var2; i++ ))
 	do
-	ssh pi@"10.1.1.$i" nmcli dev wifi
+	ssh ucanlab@"10.1.1.$i" /usr/sbin/iwgetid -r
 	done
 	;;
 	# In order to check the SSID's of the an individual node in our system , run this command with the flag -l and the the specific node
@@ -39,12 +52,10 @@ case "$1" in
 	for (( i=1; i<=${#new[@]}-1; i++))
 	do
 	i2=$i+1
-	ssh pi@"10.1.1.${new[i]}" nmcli dev wifi
+	ssh ucanlab@"10.1.1.${new[i]}" /usr/sbin/iwgetid -r
 	done
 	;;
 esac
 break
 done
 fi
-
-
