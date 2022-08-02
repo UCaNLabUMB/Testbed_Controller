58ade843c330aa1a0689d08854dcb78248ba8f02
victoriaplanchart <planchartvictoria@gmail.com>
2022-08-02 14:16:12 -0400

    Added help function

--- Testbed_Scripts/Configuration/get_mac.sh
+++ Testbed_Scripts/Configuration/get_mac.sh
@@ -9,13 +9,19 @@
 #
 # Input: bash get_mac.sh enter, then enter the IP address suffix. 
 # Example: for IP:10.1.1.102 enter 102. For multiple IP addresses enter 102 103 104
-# 	   for specific network interface enter parameter "-n" followed by either eth0, eth1 or wlan0. e.g: bash get_mac.sh -n eth1
+#	   for a specific network interface parameter, enter "-n" followed by either eth0, eth1 or wlan0. e.g: bash get_mac.sh -n eth1
 #
 ####################################################################
 
 
 echo "Input pi's IP address suffix to get MAC Address. For muliple IPs enter them separated by a space."
 echo "For example: For IP 10.1.1.103, enter 103. For multiple addresses type: 102 103 105 106"
+
+help()
+{
+	echo "	-n [OPTIONAL] = input the network interface (eth1, eth0 or wlan0)"
+	echo "	If no input is entered, the deffault network interface will be eth0"
+}
 
 read -a ip
 
@@ -30,7 +36,7 @@ while getopts 'hn:' OPTION; do
 	esac
 done
 
-for i in "${ip[@]}" # loop through pis
+for i in "${ip[@]}"
 do
 # if statement checks which pi is in server mode by looking for eth1 address
 if ssh ucanlab@"10.1.1.$i" [ ! -d /sys/class/net/$network_interface ]
