ea68be4d7219b6c143cc93cf67d82ec398b17abc
victoriaplanchart <planchartvictoria@gmail.com>
2022-08-03 15:32:40 -0400

    Added header and help function

--- Testbed_Scripts/Configuration/legacy/get_info.sh
+++ Testbed_Scripts/Configuration/legacy/get_info.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-while getopts ":a:os:ram:memory:" options; do
+#################################################################### 
+# get_info.sh
+#
+# Initial Authors: Humza Ali / Myles Toole
+#
+# Description: This script shows IP address, OS version, RAM memory and disk space info of the given node.
+#
+# Input: bash get_info.sh -a 101, or -os 101, etc..
+#
+# **For Help, enter -h**
+#
+####################################################################
+
+help()
+{
+	echo "-a = input the node's IP suffix. Ex: 103"
+	echo "-os = shows the installed operating system's version"
+	echo "-ram = shows the free and used ram memory info"
+	echo "-memory = shows the amount of free disk space available"
+}
+
+
+while getopts ":a:os:ram:memory:h" options; do
 	case "${options}" in 
 		a ) ip=${OPTARG}
 		    echo $ip
@@ -15,6 +37,6 @@ while getopts ":a:os:ram:memory:" options; do
 		
 		memory ) df -h
            	    ;;
+           	h ) help;;
 	esac
 done
-
