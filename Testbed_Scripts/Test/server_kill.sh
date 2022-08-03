441462cdf2274da5b31aa713887ba094b78b92ce
victoriaplanchart <planchartvictoria@gmail.com>
2022-08-03 16:05:22 -0400

    Fixed command line arguments

--- Testbed_Scripts/Test/server_kill.sh
+++ Testbed_Scripts/Test/server_kill.sh
@@ -5,10 +5,9 @@
 #
 # Author: Victoria Planchart
 #
-# Description: This script kills the open ports in the specified pi.
+# Description: This script kills the open ports in the specified server node.
 #
-# Input:  enter username and IP suffix with their respective flags followed by the ports' number to be killed separated by a space.
-# example:bash server_kill.sh -u ucanlab -a 201 1168 1125 1142 1136
+# Input: bash server_kill.sh -u ucanlab (OPTIONAL) -a 201 <<< "1168 1125 1142 1136"
 #
 # **For Help, enter -h**
 #
@@ -18,11 +17,11 @@
 help()
 {
 	echo "-a = input the suffix of the pi's IP address. (-a 201 for IP: 10.1.1.201)"
-	echo "-u = input pi's username"
+	echo "-u [OPTIONAL] = input pi's username"
 	echo "enter ports' numbers separted by a space"
 }
 
-uname=ucanlab
+uname=ucanlab # deffault
 
 while getopts 'ha:u' OPTION; do
 	case "$OPTION" in
@@ -35,4 +34,6 @@ while getopts 'ha:u' OPTION; do
 	esac
 done
 
-ssh $uname@10.1.1.$ip kill -9 $3 $4 $5 $6 $7 $8 $9
+read -a ip
+
+ssh $uname@10.1.1.$ip kill -9 ${ip[@]}
