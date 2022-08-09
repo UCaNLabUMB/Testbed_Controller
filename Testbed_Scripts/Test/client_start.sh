021b17399e3d89812fd2590b588f8f1f55d062c2
victoriaplanchart <planchartvictoria@gmail.com>
2022-08-09 12:27:00 -0400

    Cleaned script and added comments

--- Testbed_Scripts/Test/client_start.sh
+++ Testbed_Scripts/Test/client_start.sh
@@ -5,15 +5,15 @@
 #
 # Author: Victoria Planchart
 #
-# Description: This script starts a number of clients and save the iperf results in a folder in the pis. It will check if the given folder
-# 		already exists in the pis. If they don't exist, it will create the folder and store the iperf results there. If the folder exists,
-# 		it will display an error message indicating on how many pis the folder exists in, and the iperf test will not run. 
-#		It takes the IP address suffix of the pis, the IP address suffix of the server, the duration of the test in seconds, the number
-#		of trials to be run and the name of the folder where the results will be saved in the pi. The script will run with the deffault
-#		username, in this case "ucanlab", in case of another please specify using its respective flag.
+# Description: This script starts a number of clients and save the iperf results in a folder in the RPis. It will check if the given folder
+# 		already exists in the RPis; if they don't exist, it will create the folder and store the iperf results. If the folder exists,
+# 		it will display an error message indicating on how many RPis the folder exists in, and the iperf test will not run. 
+#		It takes the IP address suffix of the RPis, the IP address suffix of the server, the duration of the test in seconds, the number
+#		of trials to be run and the name of the folder where the results will be saved in the RPpi. The script will run with the deffault
+#		username, in this case "ucanlab"; if using a different username, please specify using its respective flag -u.
 #
-# Input: bash client_start.sh -n 3 -i 201 -t 5 -l 5 -f <<folder name>> -s <<< "server address" or
-#	 bash client_start.sh -a <<< "103 106 108 112" -i 201 -t 5 -l 5 -f <<folder name>> -s <<< "server address"
+# Input: bash client_start.sh -n 3 -i 201 -t 5 -l 5 -f <folder name> -s "server address" or
+#	 bash client_start.sh -a "103 106 108 112" -i 201 -t 5 -l 5 -f <folder name> -s "server address"
 #
 # **For Help, enter -h**
 #
@@ -35,28 +35,17 @@ help()
 	echo "	-l = input number of iterations for the test to run"
 }
 
-#setup_server_array()
-#{
-#	read -a my_input
-#	
-#	if [ ${#my_input[@]} -eq 1 ]; then # checks if the array equals to 1
-#		i=0
-#		while [ $i -lt ${#my_address[@]} ]; do
-#			my_server_array[$i]=$my_input # assigns the only input value to the array of lentgh N
-#			i=$((i + 1))
-#		done
-#	else
-#		my_server_array=("${my_input[@]}") # creates array with more than one value
-#	fi
-#}
 
-address_single_setup() # takes array from base_address to base_address + 1. e.g if N=3, then 101, 102, 103 clients will start.
+# takes array from base_address to base_address + 1. e.g if N=3, then 101, 102, 103 clients will start.
+address_single_setup() 
 {
 	N=$OPTARG
 	base_address=100
 	my_address=($(seq $((base_address+1)) 1 $((base_address+$N))))
 }
 
+
+# reads "my_address_array", remove the blank spaces and creates a new "my_addess" array.
 address_array_setup()
 {
 	my_address_array=$OPTARG
@@ -65,6 +54,8 @@ address_array_setup()
 	read -ra my_address <<< "$my_address_array"
 }
 
+
+# reads "setup_server_array", remove the blank spaces and creates a new "temp_server_array" array.
 server_array_setup()
 {
 	setup_server_array=$OPTARG
@@ -72,6 +63,7 @@ server_array_setup()
 	IFS=' '
 	read -ra temp_server_array <<< "$setup_server_array"
 }
+
 
 #### DEFFAULT VALUES
 
@@ -104,52 +96,57 @@ done
 
 #### MAIN CODE
 
+# while loop creates values for temp_number by subtracting 100 from the entered IP (103 106 112 will create values 3 6 12)
+# creates values for my_ports by adding 5200 and the temp_number. my_s values will equal temp_number values.
 i=0
 while [[ $i -lt ${#my_address[@]} ]]; do
 	temp_number=$((${my_address[$i]}-100))
 	my_ports[$i]=$((5200+$temp_number))
-	my_s[$i]=$temp_number
-	
+	my_s[$i]=$temp_number	
 	i=$((i + 1))
 done
 
-if [ ${#temp_server_array[@]} -eq 1 ]; then # checks if the array equals to 1
+
+# if statement checks if the array length equals to 1
+if [ ${#temp_server_array[@]} -eq 1 ]; then
 	i=0
-	while [ $i -lt ${#my_address[@]} ]; do
-		my_server_array[$i]=$temp_server_array # assigns the only input value to the array of lentgh N
+	while [ $i -lt ${#my_address[@]} ]; do # if the array length is 1, it assigns the only value to the array of lentgh $my_address
+		my_server_array[$i]=$temp_server_array
 		i=$((i + 1))
 	done
 else
-	my_server_array=("${temp_server_array[@]}") # creates array with more than one value
+	my_server_array=("${temp_server_array[@]}") # if the array length is more than 1, it creates the array with more than one value.
 fi
+
 
 i=0
 error=0
-while [[ $i -lt ${#my_address[@]} ]]; do # loop for number of clients
+while [[ $i -lt ${#my_address[@]} ]]; do # loop through number of clients
 
-if ssh $uname@10.1.1.${my_address[$i]} [ ! -d ~/TB_Results/${folder_name}_pi${my_address[$i]} ] # checks if the given directory folder exists in the pis
+if ssh $uname@10.1.1.${my_address[$i]} [ ! -d ~/TB_Results/${folder_name}_pi${my_address[$i]} ] # checks if the given directory folder exists in the client nodes
 then
 	echo "Directory does not exist in pi ${my_address[$i]}"
-	ssh $uname@10.1.1.${my_address[$i]} mkdir -p ~/TB_Results/${folder_name}_pi${my_address[$i]} # if they don't exist, it creates the given directory
+	ssh $uname@10.1.1.${my_address[$i]} mkdir -p ~/TB_Results/${folder_name}_pi${my_address[$i]} # if it does not exist, it creates the given directory
 	
 else
-	echo "Directory exists in pi ${my_address[$i]}" # if they exist, the error count goes up by one
+	echo "Directory exists in pi ${my_address[$i]}" # if it exists, the error count goes up by one
 	error=$((error + 1))
 fi
-
 i=$((i + 1))
 done
 
 
-if [ $error -lt 1 ] # if the error count is zero, then the iperf test in the following loop will be executed
+# if statement will check if the error count is zero
+if [ $error -lt 1 ] # if it's zero, the test in the while loop will run
 then
 
 x=0
-while [[ $x -lt $trial ]] # loop for number of trials
+while [[ $x -lt $trial ]] # loop through number of trials
 do
 	x=$(($x+1))
 	i=0
-	while [[ $i -lt ${#my_address[@]} ]]; do # loop for number of clients
+	while [[ $i -lt ${#my_address[@]} ]]; do # loop through number of clients
+		# ssh in the client nodes, start the clients and run the test
 		ssh $uname@10.1.1.${my_address[$i]}  "iperf3 -c 192.168.${my_server_array[$i]}.$ip -t $time s${my_s[$i]} -p ${my_ports[$i]} > ~/TB_Results/${folder_name}_pi${my_address[$i]}/Results_$x.txt" &	
 	i=$((i + 1))
 	done
@@ -157,9 +154,8 @@ do
 	sleep 5
 done
 echo "Test is completed"
-# if the error count is greater than zero, the iperf test will not be executed and will display a message indicating how many pis the directory already exists in
+# if the error count is greater than zero, the iperf test will not be executed and will display a message indicating how many RPis the directory already exists in
 else
 	echo "Directory exists in $error pis"
 
 fi
-
