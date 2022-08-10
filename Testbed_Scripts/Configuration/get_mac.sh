7d30477c1db019e6cdfe2261c9b6d0923eed748e
victoriaplanchart <planchartvictoria@gmail.com>
2022-08-10 13:58:51 -0400

    Updated range format

--- Testbed_Scripts/Configuration/get_mac.sh
+++ Testbed_Scripts/Configuration/get_mac.sh
@@ -7,61 +7,84 @@
 #
 # Description: This script indicates the eth0, eth1 or wlan0 MAC address of the given node's IP addresses.
 #
-# Input: bash get_mac.sh -n (either eth0, eth1 or wlan0) -a (for specific nodes) or -r (for node's range)
-# Example: for range from 101 to 115 type -r 101 115
-#	   for specific nodes type -a, enter, 101 104 107 112 115
+# Input: bash get_mac.sh -n (either eth0, eth1 or wlan0) -l (for specific nodes) or -r (for node's range)
+# Example: for range from 101 to 115 type -r 101,115
+#	   for specific nodes type -l 101,104,107,112,115
 #	   for a specific network interface parameter, enter "-n" followed by either eth0, eth1 or wlan0. e.g: bash get_mac.sh -n eth1
 #
 ####################################################################
 
+#-------------------------------------------------------------------
+
 help()
 {
-	echo "	-n = input the network interface (eth1, eth0 or wlan0)"
-	echo "	-r = input the maximum and minimum values of the node range. Ex: For nodes 101 through 106, type -r 101 106"
-	echo "	-a = type in -a with no inputs, enter, and then enter specific RPis (101 103 105 108)"
+	echo "	### Bash script to get eth1, eth0 or wlan0 MAC addresses for testbed nodes ###"
+	echo "	---------------------------------------------------------------------------------------"
+	echo "	-n = input the network interface eth1, eth0 or wlan0 (e.g., bash get_mac.sh -n eth0)"
+	echo "	-r = range of testbed node addresses (e.g., bash get_mac.sh -r 103,107)"
+	echo "	-l = list of testbed node addresses (e.g., bash get_mac.sh -l 103,105,109)"
 	echo "	-u [OPTIONAL] = input client's username if the deffault one (ucanlab) is not used"
+	exit
 }
 
-# creates array from command line
-no_range()
+#-------------------------------------------------------------------
+
+# Creates array from command line inputs
+addresses_no_range()
 {
-	read -a ip
+	IFS=','
+	read -ra ip <<< "$OPTARG"
 }
 
-# creates array from $var1 to $var2
-range()
+#-------------------------------------------------------------------
+
+# Parse input and create ip array from arg1 through arg2
+addresses_range()
 {
-	for (( i=$var1; i<=$var2; i++ ))
+	IFS=','
+	read -ra temp <<< "$OPTARG"
+	index=0
+	for (( i=${temp[0]}; i<=${temp[1]}; i++ ))
 	do
-		ip[$i]=$i
+		ip[$index]=$i
+		index=$((index+1))
 	done
 }
 
-var1="$4"; 
-var2="$5";
-i=$var1
+#-------------------------------------------------------------------
+# Set default parameters
 
-uname=ucanlab # deffault
+uname=ucanlab # default
 
-while getopts 'hn:ur:a' OPTION; do
+#-------------------------------------------------------------------
+# Get arguments and set appropriate parameters
+
+while getopts 'hn:ur:l:' OPTION; do
 	case "$OPTION" in
 		h)
 			help;;
 		n)
 			network_interface=$OPTARG;;
 		r)
-			range;;
-		a)
-			no_range;;
+			addresses_range;;
+		l)
+			addresses_no_range;;
 		u)
 			uname=$OPTARG;;
 	esac
 done
 
+
+#############################
+#####     Main Code     #####
+#############################
+#-------------------------------------------------------------------
+
 i=0
 for i in "${ip[@]}"
 do
-# if statement checks which pi is in server mode by looking for eth1 address
+
+# if statement checks which RPi is in server mode by looking for eth1 address
 if ssh ucanlab@"10.1.1.$i" [ ! -d /sys/class/net/$network_interface ]
 then
 	echo "Network Interface not found for $i"
@@ -71,3 +94,4 @@ else
 fi
 i=$((i + 1))
 done
+
