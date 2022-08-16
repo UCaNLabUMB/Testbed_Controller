5b67e1617ef193e7b0723bfe6d1af13dafcd98bd
Michael Rahaim <rahaimm@gmail.com>
2022-08-16 13:23:25 -0400

    merged server status scripts
    added kill pid option to server_status and removed server_kill script, since all other functionality is equivalent

--- Testbed_Scripts/Test/server_status.sh
+++ Testbed_Scripts/Test/server_status.sh
@@ -25,6 +25,9 @@ help()
 	echo "	### Bash script to check for active iperf servers (pid shown in last col) ###"
 	echo "	---------------------------------------------------------------------------------------"
 	echo "	-a = input the suffix of the pi's IP address. (-a 101 for IP: 10.1.1.101)"
+	echo "	-k [OPTIONAL] = kill server processes with specified PID "
+	echo "	                  (e.g., bash server_status.sh -a 101 -k 1234,1235) "
+	echo "	                  CAUTION: can kill other processes if incorrect pid is given"
 	echo "	-u [OPTIONAL] = client's username (e.g., '-u uname') (default: ucanlab)"
 	echo ""
 	exit
@@ -39,16 +42,21 @@ help()
 # Set default parameters
 
 uname=ucanlab # default
+kill_procs=0
 
 #------------------------------------------------------------------------------------
 # Get arguments and set appropriate parameters
 
-while getopts 'ha:u:' OPTION; do
+while getopts 'ha:k:u:' OPTION; do
 	case "$OPTION" in
 		h)
 			help;;
 		a)
 			ip=$OPTARG;;
+		k)
+			kill_procs=1;
+			IFS=','
+			read -ra pids <<< "$OPTARG";;
 		u)
 			uname=$OPTARG;;
 	esac
@@ -61,4 +69,12 @@ done
 #############################
 #------------------------------------------------------------------------------------
 
+if (( $kill_procs == 1 ))
+then
+	ssh $uname@10.1.1.$ip kill -9 ${pids[@]}
+	sleep 1
+fi
+
 ssh $uname@10.1.1.$ip netstat -l -p | grep iperf
+
+
